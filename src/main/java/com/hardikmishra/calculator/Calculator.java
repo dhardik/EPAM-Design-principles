8de4a51589994e629764bd181b8024831d68f97b
dhardik <35969168+dhardik@users.noreply.github.com>
2020-02-12 00:19:23 +0530

    Add files via upload

---
+++ src/main/java/com/hardikmishra/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.hardikmishra.calculator;
+
+import com.hardikmishra.evaluator.*;
+
+public class Calculator
+{
+    private Evaluator evaluator;
+  
+    public Calculator()
+    {
+        evaluator=new Evaluator();
+    }
+  
+    public void solve(double num1,double num2,char expr) 
+    {
+      evaluator.evaluate(num1, num2, expr);
+      evaluator.displayResult();
+    }
+}
